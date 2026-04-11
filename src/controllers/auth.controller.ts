7d4f06bb6062d7e8a34276eb78f2d89fab80b5c7
skp544 <shubhamkumarprajapati544@gmail.com>
2026-04-11 11:32:10 +0530

    feat: implement student and mentor signup, login, OTP sending and verification endpoints; refactor routes to use controller methods

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -12,6 +12,13 @@
 //   uploadDocumentsService,
 // } from "../services/mentor.service";
 
+import { RequestHandler } from "express";
+import { prisma } from "../lib/prisma";
+import { comparePassword, hashPassword } from "../utils/hash-password";
+import { errorHandler, successHandler } from "../utils/api-handlers";
+import { STATUS_CODES } from "../constants/status-codes";
+import { jwtToken } from "../utils/jwt";
+
 // export const generateOtpController: RequestHandler = async (req, res) => {
 //   try {
 //     const { identifier, identifierType } = req.body;
@@ -151,3 +158,216 @@
 //     });
 //   }
 // };
+
+export const signUpStudent: RequestHandler = async (req, res) => {
+  try {
+    const { email, password, name } = req.body;
+
+    const existing = await prisma.user.findUnique({ where: { email } });
+
+    if (existing)
+      return errorHandler({
+        statusCode: STATUS_CODES.BAD_REQUEST,
+        res,
+        message: "User already exists!",
+      });
+
+    const hashed = await hashPassword(password);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashed,
+        role: "STUDENT",
+        studentProfile: {
+          create: { name },
+        },
+      },
+    });
+
+    return successHandler({
+      statusCode: STATUS_CODES.CREATED,
+      res,
+      message: "Student created",
+      data: { userId: user.id },
+    });
+  } catch (error) {
+    return errorHandler({
+      res,
+      error,
+    });
+  }
+};
+
+export const signUpMentor: RequestHandler = async (req, res) => {
+  try {
+    const { email, password, name } = req.body;
+
+    const existing = await prisma.user.findUnique({ where: { email } });
+
+    if (existing)
+      return errorHandler({
+        statusCode: STATUS_CODES.BAD_REQUEST,
+        res,
+        message: "User already exists!",
+      });
+
+    const hashed = await hashPassword(password);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashed,
+        role: "MENTOR",
+        mentorProfile: {
+          create: { name },
+        },
+      },
+    });
+
+    return successHandler({
+      statusCode: STATUS_CODES.CREATED,
+      res,
+      message: "Mentor created",
+      data: { userId: user.id },
+    });
+  } catch (error) {
+    return errorHandler({
+      res,
+      error,
+    });
+  }
+};
+
+export const login: RequestHandler = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+      include: {
+        student_profile: true,
+        mentor_profile: true,
+      },
+    });
+
+    if (!user)
+      return errorHandler({
+        statusCode: STATUS_CODES.BAD_REQUEST,
+        res,
+        message: "User not found!",
+      });
+
+    if (!user.password)
+      return errorHandler({
+        statusCode: STATUS_CODES.UNPROCESSABLE_ENTITY,
+        res,
+        message: "Invalid credentials",
+      });
+
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch)
+      return errorHandler({
+        statusCode: STATUS_CODES.BAD_REQUEST,
+        res,
+        message: "Invalid credentials",
+      });
+
+    const token = jwtToken.sign(user);
+
+    return successHandler({
+      res,
+      message: "Login successful",
+      data: {
+        token,
+        user: {
+          id: user.id,
+          email: user.email,
+          role: user.role,
+          profile:
+            user.role === "STUDENT"
+              ? user.student_profile
+              : user.mentor_profile,
+        },
+      },
+    });
+  } catch (err) {
+    return errorHandler({ res, error: err });
+  }
+};
+
+export const sendOtp: RequestHandler = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user)
+      return errorHandler({
+        statusCode: STATUS_CODES.NOT_FOUND,
+        res,
+        message: "User not found",
+      });
+
+    const code = Math.floor(1000 + Math.random() * 9000).toString();
+
+    await prisma.otp.upsert({
+      where: { userId: user.id },
+      update: {
+        code,
+        expiresAt: new Date(Date.now() + 5 * 60 * 1000),
+      },
+      create: {
+        code,
+        userId: user.id,
+        expiresAt: new Date(Date.now() + 5 * 60 * 1000),
+      },
+    });
+
+    return successHandler({
+      res,
+      message: "OTP sent",
+      data: { otp: code },
+    });
+  } catch (error) {
+    return errorHandler({ res, error });
+  }
+};
+
+export const verifyOtp: RequestHandler = async (req, res) => {
+  try {
+    const { email, code } = req.body;
+
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user)
+      return errorHandler({
+        statusCode: STATUS_CODES.NOT_FOUND,
+        res,
+        message: "User not found",
+      });
+
+    const otp = await prisma.otp.findUnique({ where: { userId: user.id } });
+
+    if (!otp || otp.code !== code || otp.expiresAt < new Date()) {
+      return errorHandler({
+        statusCode: STATUS_CODES.BAD_REQUEST,
+        res,
+        message: "Invalid OTP",
+      });
+    }
+
+    await prisma.user.update({
+      where: { id: user.id },
+      data: { isVerified: true },
+    });
+
+    await prisma.otp.delete({ where: { userId: user.id } });
+
+    return successHandler({
+      res,
+      message: "Verified successfully",
+    });
+  } catch (error) {
+    return errorHandler({ res, error });
+  }
+};
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-// import {
-//   generateOtpController,
-//   verifyOtpController,
-// } from "../controllers/auth.controller";
-import { prisma } from "../lib/prisma";
-import { comparePassword, hashPassword } from "../utils/hash-password";
-import { jwtToken } from "../utils/jwt";
+import {
+  login,
+  sendOtp,
+  signUpMentor,
+  signUpStudent,
+  verifyOtp,
+} from "../controllers/auth.controller";
 
 const authRouter = Router();
 
@@ -14,140 +14,14 @@ const authRouter = Router();
 
 // new routes
 
-authRouter.post("signup/student", async (req, res) => {
-  try {
-    const { email, password, name } = req.body;
+authRouter.post("signup/student", signUpStudent);
 
-    const existing = await prisma.user.findUnique({ where: { email } });
-    if (existing) return res.status(400).json({ message: "User exists" });
+authRouter.post("/signup/mentor", signUpMentor);
 
-    const hashed = await hashPassword(password);
+authRouter.post("/login", login);
 
-    const user = await prisma.user.create({
-      data: {
-        email,
-        password: hashed,
-        role: "STUDENT",
-        studentProfile: {
-          create: { name },
-        },
-      },
-    });
+authRouter.post("/send-otp", sendOtp);
 
-    res.json({ message: "Student created", userId: user.id });
-  } catch (error) {}
-});
-
-authRouter.post("/signup/mentor", async (req, res) => {
-  try {
-    const { email, password, name } = req.body;
-
-    const existing = await prisma.user.findUnique({ where: { email } });
-    if (existing) return res.status(400).json({ message: "User exists" });
-
-    const hashed = await hashPassword(password);
-
-    const user = await prisma.user.create({
-      data: {
-        email,
-        password: hashed,
-        role: "MENTOR",
-        mentorProfile: {
-          create: { name },
-        },
-      },
-    });
-
-    res.json({ message: "Mentor created", userId: user.id });
-  } catch (err) {
-    res.status(500).json({ error: "Something went wrong" });
-  }
-});
-
-authRouter.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await prisma.user.findUnique({
-      where: { email },
-      include: {
-        student_profile: true,
-        mentor_profile: true,
-      },
-    });
-
-    if (!user) return res.status(400).json({ message: "Invalid credentials" });
-
-    if (!user.password)
-      return res.status(400).json({ message: "Invalid credentials" });
-
-    const isMatch = await comparePassword(password, user.password);
-    if (!isMatch)
-      return res.status(400).json({ message: "Invalid credentials" });
-
-    const token = jwtToken.sign(user);
-
-    res.json({
-      token,
-      user: {
-        id: user.id,
-        email: user.email,
-        role: user.role,
-        profile:
-          user.role === "STUDENT" ? user.student_profile : user.mentor_profile,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({ error: "err.message" });
-  }
-});
-
-authRouter.post("/send-otp", async (req, res) => {
-  const { email } = req.body;
-
-  const user = await prisma.user.findUnique({ where: { email } });
-  if (!user) return res.status(404).json({ message: "User not found" });
-
-  const code = Math.floor(1000 + Math.random() * 9000).toString();
-
-  await prisma.otp.upsert({
-    where: { userId: user.id },
-    update: {
-      code,
-      expiresAt: new Date(Date.now() + 5 * 60 * 1000),
-    },
-    create: {
-      code,
-      userId: user.id,
-      expiresAt: new Date(Date.now() + 5 * 60 * 1000),
-    },
-  });
-
-  console.log("OTP:", code);
-
-  res.json({ message: "OTP sent" });
-});
-
-authRouter.post("/verify-otp", async (req, res) => {
-  const { email, code } = req.body;
-
-  const user = await prisma.user.findUnique({ where: { email } });
-
-  if (!user) return res.status(404).json({ message: "User not found" });
-  const otp = await prisma.otp.findUnique({ where: { userId: user.id } });
-
-  if (!otp || otp.code !== code || otp.expiresAt < new Date()) {
-    return res.status(400).json({ message: "Invalid OTP" });
-  }
-
-  await prisma.user.update({
-    where: { id: user.id },
-    data: { isVerified: true },
-  });
-
-  await prisma.otp.delete({ where: { userId: user.id } });
-
-  res.json({ message: "Verified successfully" });
-});
+authRouter.post("/verify-otp", verifyOtp);
 
 export default authRouter;
